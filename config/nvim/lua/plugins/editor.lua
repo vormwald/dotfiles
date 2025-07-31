aac38a5b631db021f4f67912e2ade77afd05ae82
Mike Vormwald <mvormwald@gmail.com>
2025-07-31 08:19:22 -0400

    messing with copilot

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -264,6 +264,15 @@ return {
     'zbirenbaum/copilot-cmp',
     config = function()
       require('copilot_cmp').setup()
+      local cmp = require 'cmp'
+
+      cmp.event:on('menu_opened', function()
+        vim.b.copilot_suggestion_hidden = true
+      end)
+
+      cmp.event:on('menu_closed', function()
+        vim.b.copilot_suggestion_hidden = false
+      end)
     end,
   },
   {
